6e693f947b5ee89de42a8ab510497a15011ab186
Hadeel <ha.ada1@hotmail.com>
2026-06-27 19:24:44 +0300

    Initial Commit

---
+++ Sanam/SanamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanamApp.swift
+//  Sanam
+//
+//  Created by Hadeel on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SanamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
